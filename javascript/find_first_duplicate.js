3aa2581ec320d61ef4c4a7be96a4bdd08c0f1d09
Andrew Pellum <apellum19@gmail.com>
2021-11-18 15:55:18 -0700

    Solved Algorithm

--- javascript/find_first_duplicate.js
+++ javascript/find_first_duplicate.js
@@ -1,5 +1,18 @@
 function findFirstDuplicate(arr) {
-  // type your code here
+  let result
+  if (arr.length !== 0) {
+    for (let index = 0; index < arr.length; index++) {
+      const element = arr[index];
+      if (element === arr[index + 1]) {
+        return result = element
+      } else {
+        result = -1
+      }
+    }
+  } else {
+    result = -1
+  }
+  return result
 }
 
 if (require.main === module) {
@@ -11,9 +24,19 @@ if (require.main === module) {
 
   console.log("Expecting: -1");
   console.log("=>", findFirstDuplicate([1, 2, 3, 4]));
+
+  console.log("Expecting: -1");
+  console.log("=>", findFirstDuplicate([]))
+
 }
 
 module.exports = findFirstDuplicate;
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+
+//take the arr
+//test - more than 2, empty arr, multiple duplicate numbers
+
+// compare numbers ===
+// if false move on to next numbers, if true return number
